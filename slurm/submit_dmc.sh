ca685228a62f2fec98f97bf8deed3feb3ba1d8d0
Daniel Palenicek <daniel.palenicek@tu-darmstadt.de>
2024-06-03 23:56:35 +0200

    fixed env names

--- slurm/submit_dmc.sh
+++ slurm/submit_dmc.sh
@@ -8,7 +8,7 @@ q_bias=0
 # policy delay
 
 # for env in 'dm_control/ball_in_cup-catch' 'dm_control/fish-swim' 'dm_control/finger-spin' 'dm_control/reacher-easy' 'dm_control/reacher-hard' 'dm_control/walker-run'; do
-for env in 'dm_control/dog_stand' 'dm_control/humanoid_stand'; do # 'dm_control/dog_run' 'dm_control/acrobot_swingup'; do
+for env in 'dm_control/dog-stand-v0' 'dm_control/humanoid-stand-v0'; do # 'dm_control/dog_run' 'dm_control/acrobot_swingup'; do
     ALGO='sac'  ENV=$env ACT='relu' LR='0.0008'  UTD='1' PI_DELAY='1' B1='0.9' XQ_STYLE=1 BN=1 LN=0 N_NEURONS=2048 N_CRITICS=2 BN_MODE='brn_actor' BN_MOM='0.99' EVAL_QBIAS=$q_bias STEPS=$timesteps START=5000 sbatch slurm_experiment_4.sh; # CrossQ
     ALGO='sac'  ENV=$env ACT='relu' LR='0.0007'  UTD='1' PI_DELAY='3' B1='0.5' XQ_STYLE=1 BN=1 LN=0 N_NEURONS=2048 N_CRITICS=2 BN_MODE='brn_actor' BN_MOM='0.99' EVAL_QBIAS=$q_bias STEPS=$timesteps START=5000 sbatch slurm_experiment_4.sh; # CrossQ
     ALGO='sac'  ENV=$env ACT='relu' LR='0.0007'  UTD='1' PI_DELAY='3' B1='0.5' XQ_STYLE=1 BN=1 LN=0 N_NEURONS=2048 N_CRITICS=2 BN_MODE='brn_actor' BN_MOM='0.99' EVAL_QBIAS=$q_bias STEPS=$timesteps START=2500 sbatch slurm_experiment_4.sh; # CrossQ
